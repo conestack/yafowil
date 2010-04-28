0cbcb13e524894344c27c1dc8c3213c273f32fc8
Robert Niederreiter <rnix@blackbox.(none)>
2010-04-28 09:37:26 +0200

    change Controller API

--- src/yafowil/controller.py
+++ src/yafowil/controller.py
@@ -2,31 +2,30 @@ class Controller(object):
     """Form controller.
     """
     
-    def __init__(self, widget):
+    def __init__(self, widget, request):
         """Initialize controller
                     
         ``widget``
             yafowil.base.Widget tree.
         """
         self.widget = widget
+        self.request = request
         self.error = False
-    
-    def __call__(self, request):
-        """Perform form processing for widget.
-        """
+        self.next = None
         self.data = self.widget.extract(request)
-        self.error = False
         self._error(self.data)
         if self.error:
-            return None
-        #self.handle(self.widget, self.data)
+            return
         for action in self.actions:
             if self.triggered(request, action):
                 if action.attributes.get('handler'):
                     action.attributes.handler(self.widget, self.data)
                 if action.attrs.get('next'):
-                    return action.attrs.next(request)
-        return None
+                    self.next = action.attrs.next(request)
+    
+    @property
+    def rendered(self):
+        return self.form(data=self.data)
     
     @property
     def actions(self):
@@ -34,19 +33,6 @@ class Controller(object):
     
     def triggered(self, request, action):
         return request.get('action.%s' % '.'.join(action.path))
-    
-    #def handle(self, widget, data):
-    #    handler = None
-    #    for action in self.actions:
-    #        if self.triggered(data['request'], action):
-    #            if action.attributes.get('handler'):
-    #                handler = action.attributes.handler
-    #                break
-    #    if handler:
-    #        handler(widget, data)
-    #    for sub in widget.values():
-    #        #  XXX: delegation in data needed
-    #        self.handle(sub, data)
     
     def _error(self, data):
         if data.get('errors'):
