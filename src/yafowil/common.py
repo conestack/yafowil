8cfc77d8d330172b0133874afa1ab6f01bf0e6e1
Robert Niederreiter <rnix@squarewave.at>
2010-12-12 16:20:24 +0100

    cleanup in common.py and common.txt, improve password widget

--- src/yafowil/common.py
+++ src/yafowil/common.py
@@ -35,7 +35,13 @@ factory.document['required_class_default'] = """\
     CSS-class to put on in case if required condition was not met if no specific 
     class was given."""
 
+###############################################################################
+# generic
+###############################################################################
+
 def _value(widget, data):
+    """XXX: move to utils.
+    """
     if data.extracted is not UNSET:
         return data.extracted
     if data.value is not UNSET:
@@ -89,7 +95,11 @@ class InputGenericPreprocessor(object):
     def __call__(self, widget, data):
         data.attrs['input_field_type'] = self.inputtype   
         return data
-    
+
+###############################################################################
+# text and hidden
+###############################################################################
+
 def register_generic_input(subtype, enable_required_class=True):
     if enable_required_class:
         factory.defaults['%s.required_class' % subtype] = 'required'
@@ -103,39 +113,9 @@ def register_generic_input(subtype, enable_required_class=True):
 register_generic_input('text')
 register_generic_input('hidden', False)
 
-def minlength_extractor(widget, data):
-    val = data.extracted
-    minlength = widget.attrs.get('minlength', -1)
-    if minlength != -1:
-        if len(val) < minlength:
-            message = u'Input must have at least %i characters.' % minlength
-            raise ExtractionError(message)
-    return val
-
-def ascii_extractor(widget, data):
-    val = data.extracted
-    if not widget.attrs.get('ascii', False):
-        return val
-    try:
-        str(val)
-    except UnicodeEncodeError:
-        raise ExtractionError(u'Input contains illegal characters.')
-    return val
-
-factory.defaults['password.required_class'] = 'required'
-factory.defaults['password.default'] = ''
-factory.defaults['password.class'] = 'password'
-factory.defaults['password.minlength'] = -1
-factory.defaults['password.ascii'] = False
-factory.register('password', 
-                 [
-                     generic_extractor,
-                     generic_required_extractor,
-                     minlength_extractor,
-                     ascii_extractor
-                 ],
-                 [input_generic_renderer],
-                 [InputGenericPreprocessor('password')])
+###############################################################################
+# proxy 
+###############################################################################
 
 @managedprops(*css_managed_props)
 def input_proxy_renderer(widget, data):
@@ -156,6 +136,133 @@ def input_proxy_renderer(widget, data):
 factory.register('proxy', 
                  [generic_extractor], 
                  [input_proxy_renderer])
+
+###############################################################################
+# textarea
+###############################################################################
+
+def textarea_renderer(widget, data):
+    tag = data.tag
+    css = widget.attrs.get('css', list())
+    if isinstance(css, basestring):
+        css = [css]
+    area_attrs = {
+        'name_': widget.dottedpath,
+        'id': cssid(widget, 'input'),
+        'class_': cssclasses(widget, data, *css),            
+        'wrap': widget.attrs['wrap'],
+        'cols': widget.attrs['cols'],
+        'rows': widget.attrs['rows'],
+        'readonly': widget.attrs['readonly'] and 'readonly',
+    }
+    value = _value(widget, data)
+    if not value:
+        value = ''
+    return tag('textarea', value, **area_attrs)
+
+factory.defaults['textarea.default'] = ''          
+factory.defaults['textarea.wrap'] = None          
+factory.defaults['textarea.cols'] = 80          
+factory.defaults['textarea.rows'] = 25          
+factory.defaults['textarea.readonly'] = None          
+factory.register('textarea', 
+                 [generic_extractor, generic_required_extractor], 
+                 [textarea_renderer])
+
+###############################################################################
+# password
+###############################################################################
+
+def minlength_extractor(widget, data):
+    val = data.extracted
+    if val is UNSET:
+        return val
+    minlength = widget.attrs.get('minlength', -1)
+    if minlength != -1:
+        if len(val) < minlength:
+            message = u'Input must have at least %i characters.' % minlength
+            raise ExtractionError(message)
+    return val
+
+def ascii_extractor(widget, data):
+    val = data.extracted
+    if val is UNSET:
+        return val
+    if not widget.attrs.get('ascii', False):
+        return val
+    try:
+        str(val)
+    except UnicodeEncodeError:
+        raise ExtractionError(u'Input contains illegal characters.')
+    return val
+
+LOWER_CASE_RE = '(?=.*[a-z])'
+UPPER_CASE_RE = '(?=.*[A-Z])'
+DIGIT_RE = '(?=.*[\d])'
+SPECIAL_CHAR_RE = '(?=.*[\W])'
+RE_PASSWORD_ALL = [
+    LOWER_CASE_RE,
+    UPPER_CASE_RE,
+    DIGIT_RE,
+    SPECIAL_CHAR_RE]
+PASSWORD_NOCHANGE_VALUE = '_NOCHANGE_'
+
+def password_extractor(widget, data):
+    val = data.extracted
+    if val == PASSWORD_NOCHANGE_VALUE:
+        return UNSET
+    if val is UNSET:
+        return val
+    required_strength = widget.attrs.get('strength', 0)
+    if not required_strength:
+        return val
+    if required_strength > len(RE_PASSWORD_ALL):
+        required_strength = len(RE_PASSWORD_ALL)
+    strength = 0
+    for reg_exp in RE_PASSWORD_ALL:
+        if re.match(reg_exp, val):
+            strength += 1
+    if strength < required_strength:
+        raise ExtractionError(u"Password too weak")
+    return val
+
+def password_renderer(widget, data):
+    tag = data.tag
+    css = widget.attrs.get('class', list())
+    if isinstance(css, basestring):
+        css = [css]
+    def pwd_value(widget, data):
+        if data.extracted is not UNSET:
+            return data.extracted
+        if data.value is not UNSET \
+          and data.value is not None:
+            return PASSWORD_NOCHANGE_VALUE
+        return widget.attrs['default']
+    value = pwd_value(widget, data) 
+    input_attrs = {
+        'type': 'password',
+        'value':  value,
+        'name_': widget.dottedpath,
+        'id': cssid(widget, 'input'),    
+        'class_': cssclasses(widget, data, *css),
+        'disabled': widget.attrs.get('disabled'),
+    }
+    return tag('input', **input_attrs)
+
+factory.defaults['password.required_class'] = 'required'
+factory.defaults['password.default'] = ''
+factory.defaults['password.class'] = 'password'
+factory.defaults['password.minlength'] = -1
+factory.defaults['password.ascii'] = False
+factory.register('password', 
+                 [generic_extractor, generic_required_extractor,
+                  minlength_extractor, ascii_extractor, password_extractor],
+                 [password_renderer],
+                 [])
+
+###############################################################################
+# checkbox
+###############################################################################
 
 @managedprops('format')
 def input_checkbox_extractor(widget, data):
@@ -203,70 +310,10 @@ factory.defaults['checkbox.required_class'] = 'required'
 factory.register('checkbox', 
                  [input_checkbox_extractor, generic_required_extractor], 
                  [input_checkbox_renderer])
-    
-def file_extracor(widget, data):
-    name = widget.dottedpath
-    if name not in data.request:
-        return UNSET
-    if '%s-action' % name in data.request:
-        option = data.request.get('%s-action' % name, 'keep')
-        if option == 'keep':
-            return data.value
-        elif option == 'delete':
-            return UNSET
-    return data.request[name]
 
-@managedprops('css', 'accept',*css_managed_props)
-def input_file_renderer(widget, data):
-    tag = data.tag
-    css = widget.attrs.get('css', list())
-    if isinstance(css, basestring):
-        css = [css]
-    input_attrs = {
-        'name_': widget.dottedpath,
-        'id': cssid(widget, 'input'),
-        'class_': cssclasses(widget, data, *css),            
-        'type': 'file',
-        'value':  '',
-    }
-    if widget.attrs.get('accept'):
-        input_attrs['accept'] = widget.attrs['accept']
-    return tag('input', **input_attrs)
-
-@managedprops('css', 'vocabulary', *css_managed_props)
-def file_options_renderer(widget, data):
-    if data.value in [None, UNSET, '']:
-        return data.rendered
-    tag = data.tag
-    if data.request:
-        value = [data.request.get('%s-action' % widget.dottedpath, 'keep')]
-    else:
-        value = ['keep']
-    tags = []
-    for key, term in vocabulary(widget.attrs.get('vocabulary', [])):
-        attrs = {
-            'type': 'radio',
-            'value':  key,
-            'checked': (key in value) and 'checked' or None,
-            'name_': '%s-action' % widget.dottedpath,
-            'id': cssid(widget, 'input', key),    
-            'class_': cssclasses(widget, data),    
-        }
-        input = tag('input', **attrs)
-        text = tag('span', term)
-        tags.append(tag('div', input, text, 
-                        **{'id': cssid(widget, 'radio', key)}))
-    return data.rendered + u''.join(tags)
-    
-factory.defaults['file.multivalued'] = False
-factory.defaults['file.vocabulary'] = [
-    ('keep', 'Keep Existing file'),
-    ('replace', 'Replace existing file'),
-    ('delete', 'Delete existing file'),
-]
-factory.register('file',
-                 [file_extracor, generic_required_extractor],
-                 [input_file_renderer, file_options_renderer])
+###############################################################################
+# selection
+###############################################################################
 
 def select_extractor(widget, data):
     extracted = generic_extractor(widget, data)
@@ -340,33 +387,77 @@ factory.register('select',
                  [select_extractor], 
                  [select_renderer])
 
-def textarea_renderer(widget, data):
+###############################################################################
+# file
+###############################################################################
+
+def file_extracor(widget, data):
+    name = widget.dottedpath
+    if name not in data.request:
+        return UNSET
+    if '%s-action' % name in data.request:
+        option = data.request.get('%s-action' % name, 'keep')
+        if option == 'keep':
+            return data.value
+        elif option == 'delete':
+            return UNSET
+    return data.request[name]
+
+@managedprops('css', 'accept',*css_managed_props)
+def input_file_renderer(widget, data):
     tag = data.tag
     css = widget.attrs.get('css', list())
     if isinstance(css, basestring):
         css = [css]
-    area_attrs = {
+    input_attrs = {
         'name_': widget.dottedpath,
         'id': cssid(widget, 'input'),
         'class_': cssclasses(widget, data, *css),            
-        'wrap': widget.attrs['wrap'],
-        'cols': widget.attrs['cols'],
-        'rows': widget.attrs['rows'],
-        'readonly': widget.attrs['readonly'] and 'readonly',
+        'type': 'file',
+        'value':  '',
     }
-    value = _value(widget, data)
-    if not value:
-        value = ''
-    return tag('textarea', value, **area_attrs)
+    if widget.attrs.get('accept'):
+        input_attrs['accept'] = widget.attrs['accept']
+    return tag('input', **input_attrs)
 
-factory.defaults['textarea.default'] = ''          
-factory.defaults['textarea.wrap'] = None          
-factory.defaults['textarea.cols'] = 80          
-factory.defaults['textarea.rows'] = 25          
-factory.defaults['textarea.readonly'] = None          
-factory.register('textarea', 
-                 [generic_extractor, generic_required_extractor], 
-                 [textarea_renderer])
+@managedprops('css', 'vocabulary', *css_managed_props)
+def file_options_renderer(widget, data):
+    if data.value in [None, UNSET, '']:
+        return data.rendered
+    tag = data.tag
+    if data.request:
+        value = [data.request.get('%s-action' % widget.dottedpath, 'keep')]
+    else:
+        value = ['keep']
+    tags = []
+    for key, term in vocabulary(widget.attrs.get('vocabulary', [])):
+        attrs = {
+            'type': 'radio',
+            'value':  key,
+            'checked': (key in value) and 'checked' or None,
+            'name_': '%s-action' % widget.dottedpath,
+            'id': cssid(widget, 'input', key),    
+            'class_': cssclasses(widget, data),    
+        }
+        input = tag('input', **attrs)
+        text = tag('span', term)
+        tags.append(tag('div', input, text, 
+                        **{'id': cssid(widget, 'radio', key)}))
+    return data.rendered + u''.join(tags)
+    
+factory.defaults['file.multivalued'] = False
+factory.defaults['file.vocabulary'] = [
+    ('keep', 'Keep Existing file'),
+    ('replace', 'Replace existing file'),
+    ('delete', 'Delete existing file'),
+]
+factory.register('file',
+                 [file_extracor, generic_required_extractor],
+                 [input_file_renderer, file_options_renderer])
+
+###############################################################################
+# submit
+###############################################################################
 
 def submit_renderer(widget, data):
     tag = data.tag
@@ -381,6 +472,31 @@ def submit_renderer(widget, data):
 
 factory.defaults['submit.action'] = None
 factory.register('submit', [], [submit_renderer])
+
+###############################################################################
+# email
+###############################################################################
+
+EMAIL_RE = u'^[a-zA-Z0-9\._\-]+@[a-zA-Z0-9\._\-]+.[a-zA-Z0-9]{2,6}$'
+
+def email_extractor(widget, data):
+    val = data.extracted
+    if not re.match(EMAIL_RE, val is not UNSET and val or ''):
+        raise ExtractionError(u'Input not a valid email address.')
+    return val
+
+factory.defaults['email.required_class'] = 'required'
+factory.defaults['email.default'] = ''
+factory.defaults['email.class'] = 'email'
+factory.register('email', 
+                 [generic_extractor, generic_required_extractor,
+                  email_extractor],
+                 [input_generic_renderer],
+                 [InputGenericPreprocessor('text')])
+
+###############################################################################
+# label
+###############################################################################
 
 def label_renderer(widget, data):
     tag = data.tag
@@ -405,6 +521,10 @@ factory.defaults['label.help'] = None
 factory.defaults['label.help_class'] = 'help'
 factory.register('label', [], [label_renderer])
 
+###############################################################################
+# field
+###############################################################################
+
 def field_renderer(widget, data):
     tag = data.tag
     div_attrs = {
@@ -419,6 +539,10 @@ factory.defaults['field.class'] = 'field'
 factory.defaults['field.witherror'] = None
 factory.register('field', [], [field_renderer])
 
+###############################################################################
+# error
+###############################################################################
+
 def error_renderer(widget, data):
     if not data.errors:
         return data.rendered
@@ -431,23 +555,3 @@ def error_renderer(widget, data):
 factory.defaults['error.error_class'] = 'error'
 factory.defaults['error.message_class'] = 'errormessage'
 factory.register('error', [], [error_renderer])
-
-EMAIL_RE = u'^[a-zA-Z0-9\._\-]+@[a-zA-Z0-9\._\-]+.[a-zA-Z0-9]{2,6}$'
-
-def email_extractor(widget, data):
-    val = data.extracted
-    if not re.match(EMAIL_RE, val is not UNSET and val or ''):
-        raise ExtractionError(u'Input not a valid email address.')
-    return val
-
-factory.defaults['email.required_class'] = 'required'
-factory.defaults['email.default'] = ''
-factory.defaults['email.class'] = 'email'
-factory.register('email', 
-                 [
-                     generic_extractor,
-                     generic_required_extractor,
-                     email_extractor
-                 ],
-                 [input_generic_renderer],
-                 [InputGenericPreprocessor('text')])
