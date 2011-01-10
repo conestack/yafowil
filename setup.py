056754aa7ed1d4c5bf3809b7b5d8c42f793749c2
Jens W. Klein <jens@bluedynamics.com>
2011-01-10 09:21:50 +0100

    version increase

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0-beta'
+version = '1.0-beta2'
 shortdesc = \
 'YAFOWIL - Yet Another Form Widget Lib: Lightweight, Extensible, Pluggable, Pure.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
