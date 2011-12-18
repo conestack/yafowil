cd6e3d2de60227f2917689e9ff7d50fc6b3cfb83
Robert Niederreiter <rnix@squarewave.at>
2011-12-18 16:19:21 +0100

    1.3pre2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.3pre1'
+version = '1.3pre2'
 shortdesc = \
 'YAFOWIL - Yet Another Form Widget Lib: Lightweight, Extensible, Pluggable, Pure.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
