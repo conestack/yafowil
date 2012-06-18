b6555bba7d8d7e2d563d460028ae356d5c5e736c
Jens W. Klein <jens@bluedynamics.com>
2012-06-18 16:49:56 +0200

    state upcoming major version change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.3.3dev'
+version = '2.0dev'
 shortdesc = \
 'YAFOWIL - declarative, flexible html forms, framework independent.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
