f859c5f31b759c70ca3d476af11a35686c3228ff
Jens W. Klein <jens@bluedynamics.com>
2012-06-21 17:41:12 +0200

    use unified plans

--- src/yafowil/example.py
+++ src/yafowil/example.py
@@ -18,40 +18,40 @@ Write me.
 
 def get_example():
     text = factory('compound', name='yafowil-text')
-    text['plain'] = factory('label:error:text', props={
+    text['plain'] = factory('#field:text', props={
         'label': 'Text Input',
     })
-    text['email'] = factory('label:error:email', props={
+    text['email'] = factory('#field:email', props={
         'label': 'E-Mail Input',
     })
-    text['number'] = factory('label:error:number', props={
+    text['number'] = factory('#field:number', props={
         'label': 'Number Input (float)',
     })
-    text['intnumber'] = factory('label:error:number', props={
+    text['intnumber'] = factory('#field:number', props={
         'label': 'Number Input (int)',
         'datatype': 'integer',
     })
-    text['password'] = factory('label:error:password', props={
+    text['password'] = factory('#field:password', props={
         'label': 'Password Input',
     })
     select = factory('compound', name='yafowil-text')
-    select['radiosingle'] = factory('label:error:select', props={
+    select['radiosingle'] = factory('#field:select', props={
         'label': 'Select (radio, single)',
         'vocabulary': ['Python 2', 'Python 3'],
         'format': 'radio',
     })
-    select['blocksingle'] = factory('label:error:select', props={
+    select['blocksingle'] = factory('#field:select', props={
         'label': 'Select (block, single)',
         'vocabulary': ['Python', 'Java', 'Perl', 'Erlang', 'C', 'C++', 'C#'],
         'format': 'block',
     })
-    select['checkboxmultiple'] = factory('label:error:select', props={
+    select['checkboxmultiple'] = factory('#field:select', props={
         'label': 'Select (multiple, checkbox)',
         'vocabulary': ['Python 2', 'Python 3'],
         'format': 'checkbox',
         'multivalued': True,
     })
-    select['blockmultiple'] = factory('label:error:select', props={
+    select['blockmultiple'] = factory('#field:select', props={
         'label': 'Select (multiple, checkbox)',
         'vocabulary': ['Python', 'Java', 'Perl', 'Erlang', 'C', 'C++', 'C#'],
         'format': 'block',
