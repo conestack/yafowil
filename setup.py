4f0ecf371baba03e7c8a0e1f27dbd74bf9339632
Robert Niederreiter <office@squarewave.at>
2012-08-16 21:25:10 +0200

    "bda.recipe.deployment pre tag commit"

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0'
+version = '2.0htug1'
 shortdesc = \
 'YAFOWIL - declarative, flexible html forms, framework independent.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
