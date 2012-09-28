a1694e6e747bc59f2024e4290726560fc130bed5
Robert Niederreiter <office@squarewave.at>
2012-09-28 14:05:41 +0200

    work on examples

--- src/yafowil/example.py
+++ src/yafowil/example.py
@@ -1,71 +1,244 @@
 from .base import factory
 
-DOC_TEXT = """\
-Text Widgets
-------------
 
-::
+DOC_PLAIN_TEXT = """\
+Plain Text
+----------
 
-  Literal Write me
+Simple plaintext widget.
+
+.. code-block:: python
+
+    plaintext = factory('#field:text', props={
+        'label': 'Plain Text Input',
+        'required': 'Input is required',
+        'help': 'Simple Plain Text Field'})
 """
 
-DOC_SELECT = """\
-Selection Widgets
------------------
+def plaintext():
+    comp = factory('compound', name='yafowil-plaintext')
+    comp['plain'] = factory('#field:text', props={
+        'label': 'Plain Text Input',
+        'required': 'Input is required',
+        'help': 'Simple Plain Text Field'})
+    return {'widget': comp,
+            'doc': DOC_PLAIN_TEXT,
+            'title': 'Plain Text'}
 
-Write me.
-"""
 
+DOC_EMAIL = """
+Email
+-----
 
-def get_example():
-    text = factory('compound', name='yafowil-text')
-    text['plain'] = factory('#field:text', props={
-        'label': 'Text Input',
-        'help': 'Simple Text Field'
-    })
-    text['email'] = factory('#field:email', props={
+Text input for email addresses.
+
+.. code-block:: python
+
+    email = factory('#field:email', props={
         'label': 'E-Mail Input',
-        'help': 'E-Mail Address Field'
-    })
-    text['number'] = factory('#field:number', props={
+        'help': 'E-Mail Address Field'})
+"""
+
+def email():
+    comp = factory('compound', name='yafowil-email')
+    comp['email'] = factory('#field:email', props={
+        'label': 'E-Mail Input',
+        'help': 'E-Mail Address Field'})
+    return {'widget': comp,
+            'doc': DOC_EMAIL,
+            'title': 'Email'}
+
+
+DOC_NUMBER = """
+Number
+------
+
+Input field accepting floating point numbers.
+
+.. code-block:: python
+
+    number = factory('#field:number', props={
         'label': 'Number Input (float)',
-        'help': 'Field for floating point number'
-    })
-    text['intnumber'] = factory('#field:number', props={
+        'help': 'Field for floating point number input'})
+"""
+
+def number():
+    comp = factory('compound', name='yafowil-number')
+    comp['number'] = factory('#field:number', props={
+        'label': 'Number Input (float)',
+        'help': 'Field for floating point number input'})
+    return {'widget': comp,
+            'doc': DOC_NUMBER,
+            'title': 'Number'}
+
+
+DOC_INT = """
+Integer
+-------
+
+Input field accepting integer values.
+
+.. code-block:: python
+
+    integer = factory('#field:number', props={
         'label': 'Number Input (int)',
         'datatype': 'integer',
-        'help': 'Field for integer'
-    })
-    text['password'] = factory('#field:password', props={
+        'help': 'Field for integer input'})
+"""
+
+def integer():
+    comp = factory('compound', name='yafowil-integer')
+    comp['intnumber'] = factory('#field:number', props={
+        'label': 'Number Input (int)',
+        'datatype': 'integer',
+        'help': 'Field for integer input'})
+    return {'widget': comp,
+            'doc': DOC_INT,
+            'title': 'Integer'}
+
+
+DOC_PASSWORD = """
+Password
+--------
+
+Password field.
+
+.. code-block:: python
+
+    password = factory('#field:password', props={
         'label': 'Password Input',
-        'help': 'Field for password'
-    })
-    select = factory('compound', name='yafowil-text')
-    select['radiosingle'] = factory('#field:select', props={
+        'help': 'Field for password'})
+"""
+
+def password():
+    comp = factory('compound', name='yafowil-password')
+    comp['password'] = factory('#field:password', props={
+        'label': 'Password Input',
+        'help': 'Field for password'})
+    return {'widget': comp,
+            'doc': DOC_PASSWORD,
+            'title': 'Password'}
+
+
+DOC_RADIO = """\
+Radio buttons
+-------------
+
+Selection with radio buttons.
+
+.. code-block:: python
+
+    radiobuttons = factory('#field:select', props={
         'label': 'Select',
-        'help': 'single as radio buttons',
+        'help': 'Single selection as radio buttons',
         'vocabulary': ['Python 2', 'Python 3'],
-        'format': 'radio',
-    })
-    select['blocksingle'] = factory('#field:select', props={
+        'format': 'radio'})
+"""
+
+def radio():
+    comp = factory('compound', name='yafowil-radio')
+    comp['radio'] = factory('#field:select', props={
         'label': 'Select',
-        'help': 'single as dropdown (aka block)',
+        'help': 'Single selection as radio buttons',
+        'vocabulary': ['Python 2', 'Python 3'],
+        'format': 'radio'})
+    return {'widget': comp,
+            'doc': DOC_RADIO,
+            'title': 'Radio buttons'}
+
+
+DOC_DROPDOWN = """
+Selection Dropdown
+------------------
+
+Single selection dropdown.
+
+.. code-block:: python
+
+    dropdown = factory('#field:select', props={
+        'label': 'Select',
+        'help': 'Single selection as dropdown',
         'vocabulary': ['Python', 'Java', 'Perl', 'Erlang', 'C', 'C++', 'C#'],
-        'format': 'block',
-    })
-    select['checkboxmultiple'] = factory('#field:select', props={
+        'format': 'block'})
+"""
+
+def dropdown():
+    comp = factory('compound', name='yafowil-dropdown')
+    comp['dropdown'] = factory('#field:select', props={
         'label': 'Select',
-        'help': 'multiple as checkboxes',
+        'help': 'Single selection as dropdown',
+        'vocabulary': ['Python', 'Java', 'Perl', 'Erlang', 'C', 'C++', 'C#'],
+        'format': 'block'})
+    return {'widget': comp,
+            'doc': DOC_DROPDOWN,
+            'title': 'Selection Dropdown'}
+
+
+DOC_CHECKBOX = """
+Checkbox selection
+------------------
+
+Multi selection with checkboxes.
+
+.. code-block:: python
+
+    checkboxes = factory('#field:select', props={
+        'label': 'Select',
+        'help': 'Multiple selection with checkboxes',
         'vocabulary': ['Python 2', 'Python 3'],
         'format': 'checkbox',
-        'multivalued': True,
-    })
-    select['blockmultiple'] = factory('#field:select', props={
+        'multivalued': True})
+"""
+
+def checkbox():
+    comp = factory('compound', name='yafowil-checkbox')
+    comp['checkbox'] = factory('#field:select', props={
+        'label': 'Select',
+        'help': 'Multiple selection with checkboxes',
+        'vocabulary': ['Python 2', 'Python 3'],
+        'format': 'checkbox',
+        'multivalued': True})
+    return {'widget': comp,
+            'doc': DOC_CHECKBOX,
+            'title': 'Checkbox selection'}
+
+
+DOC_BLOCK = """
+Selection block
+---------------
+
+Multi selection as block.
+
+.. code-block:: python
+
+    selection = factory('#field:select', props={
         'label': 'Select',
         'help': 'multiple as block',
         'vocabulary': ['Python', 'Java', 'Perl', 'Erlang', 'C', 'C++', 'C#'],
         'format': 'block',
-        'multivalued': True,
-    })
-    return [{'widget': text, 'doc': DOC_TEXT, 'title': 'Text'},
-            {'widget': select, 'doc': DOC_SELECT, 'title': 'Select'}]
+        'multivalued': True})
+"""
+
+def block():
+    comp = factory('compound', name='yafowil-block')
+    comp['block'] = factory('#field:select', props={
+        'label': 'Select',
+        'help': 'multiple as block',
+        'vocabulary': ['Python', 'Java', 'Perl', 'Erlang', 'C', 'C++', 'C#'],
+        'format': 'block',
+        'multivalued': True})
+    return {'widget': comp,
+            'doc': DOC_BLOCK,
+            'title': 'Selection block'}
+
+
+def get_example():
+    return [plaintext(),
+            email(),
+            number(),
+            integer(),
+            password(),
+            radio(),
+            dropdown(),
+            checkbox(),
+            block()]
