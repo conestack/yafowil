3859c59857f78953debe55e988d50fe9a6579b11
Robert Niederreiter <office@squarewave.at>
2015-04-12 21:17:50 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
 )
 
 
-version = '2.1.3'
+version = '2.1.4.dev0'
 shortdesc = \
 'YAFOWIL - declarative, flexible html forms, framework independent.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
