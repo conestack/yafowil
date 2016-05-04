0b30a48e07c14557977dbf00f5d61d38307ef2f8
Johannes Raggam <thetetet@gmail.com>
2016-05-04 14:44:06 +0200

    Back to development: 2.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
 )
 
 
-version = '2.1.4'
+version = '2.1.5.dev0'
 shortdesc = \
 'YAFOWIL - declarative, flexible html forms, framework independent.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
