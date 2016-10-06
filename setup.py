7e633285dcd1c590373ba98db94d73cffc3e7d32
Robert Niederreiter <office@squarewave.at>
2016-10-06 08:35:52 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '2.2'
+version = '2.2.1.dev0'
 shortdesc = \
     'YAFOWIL - declarative, flexible html forms, framework independent.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
