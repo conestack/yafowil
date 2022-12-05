1d1ea925d76e0343d61def9a19952f22740df0d4
Robert Niederreiter <rnix@squarewave.at>
2022-12-05 12:59:52 +0100

    Back to development: 3.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(name):
         return f.read()
 
 
-version = '3.0.1'
+version = '3.0.2.dev0'
 shortdesc = 'YAFOWIL - declarative, framework independent, flexible HTML forms'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
