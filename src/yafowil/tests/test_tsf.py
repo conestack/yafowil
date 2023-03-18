7d2f347885599a0132a12382c5c54aaa30059603
Robert Niederreiter <rnix@squarewave.at>
2023-03-18 10:23:42 +0100

    Update translations

--- src/yafowil/tests/test_tsf.py
+++ src/yafowil/tests/test_tsf.py
@@ -6,11 +6,11 @@ class TestTsf(NodeTestCase):
 
     def test_tsf(self):
         # Test dummy translation string factory
-        _ = DummyTranslationStringFactory('yafowil')
-        self.assertEqual(_.domain, 'yafowil')
-        self.assertEqual(_('foo'), 'foo')
-        self.assertEqual(_('bar', default=u'Bar'), 'Bar')
+        tsf = DummyTranslationStringFactory('yafowil')
+        self.assertEqual(tsf.domain, 'yafowil')
+        self.assertEqual(tsf('foo'), 'foo')
+        self.assertEqual(tsf('bar', default=u'Bar'), 'Bar')
         self.assertEqual(
-            _('baz', default=u'Baz ${bam}', mapping={'bam': 42}),
+            tsf('baz', default=u'Baz ${bam}', mapping={'bam': 42}),
             'Baz 42'
         )
