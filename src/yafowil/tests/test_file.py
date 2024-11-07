7bd7f5ad8fc39a2167cff0ba0bc7bd7464dc2eaf
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2024-11-07 11:27:08 +0100

    fix file python tests

--- src/yafowil/tests/test_file.py
+++ src/yafowil/tests/test_file.py
@@ -57,18 +57,18 @@ class TestFile(YafowilTestCase):
         self.checkOutput("""
         <div>
           <input id="input-MYFILE" name="MYFILE" type="file"/>
-          <div id="radio-MYFILE-keep">
-            <input checked="checked" id="input-MYFILE-keep"
+          <div class="" id="radio-MYFILE-keep">
+            <input checked="checked" class="" id="input-MYFILE-keep"
                    name="MYFILE-action" type="radio" value="keep"/>
             <span>Keep Existing file</span>
           </div>
-          <div id="radio-MYFILE-replace">
-            <input id="input-MYFILE-replace" name="MYFILE-action"
+          <div class="" id="radio-MYFILE-replace">
+            <input class="" id="input-MYFILE-replace" name="MYFILE-action"
                    type="radio" value="replace"/>
             <span>Replace existing file</span>
           </div>
-          <div id="radio-MYFILE-delete">
-            <input id="input-MYFILE-delete" name="MYFILE-action"
+          <div class="" id="radio-MYFILE-delete">
+            <input class="" id="input-MYFILE-delete" name="MYFILE-action"
                    type="radio" value="delete"/>
             <span>Delete existing file</span>
           </div>
@@ -128,18 +128,18 @@ class TestFile(YafowilTestCase):
         self.checkOutput("""
         <div>
           <input id="input-MYFILE" name="MYFILE" type="file"/>
-          <div id="radio-MYFILE-keep">
-            <input id="input-MYFILE-keep" name="MYFILE-action" type="radio"
-                   value="keep"/>
+          <div class="" id="radio-MYFILE-keep">
+            <input class="" id="input-MYFILE-keep" name="MYFILE-action"
+                   type="radio" value="keep"/>
             <span>Keep Existing file</span>
           </div>
-          <div id="radio-MYFILE-replace">
-            <input id="input-MYFILE-replace" name="MYFILE-action" type="radio"
-                   value="replace"/>
+          <div class="" id="radio-MYFILE-replace">
+            <input class="" id="input-MYFILE-replace" name="MYFILE-action"
+                   type="radio" value="replace"/>
             <span>Replace existing file</span>
           </div>
-          <div id="radio-MYFILE-delete">
-            <input checked="checked" id="input-MYFILE-delete"
+          <div class="" id="radio-MYFILE-delete">
+            <input checked="checked" class="" id="input-MYFILE-delete"
                    name="MYFILE-action" type="radio" value="delete"/>
             <span>Delete existing file</span>
           </div>
@@ -173,13 +173,13 @@ class TestFile(YafowilTestCase):
         self.checkOutput("""
         <div>
           <input id="input-MYFILE" name="MYFILE" type="file"/>
-          <div id="radio-MYFILE-keep">
-            <input checked="checked" id="input-MYFILE-keep"
+          <div class="" id="radio-MYFILE-keep">
+            <input checked="checked" class="" id="input-MYFILE-keep"
                    name="MYFILE-action" type="radio" value="keep"/>
             <span>Keep Existing file</span>
           </div>
-          <div id="radio-MYFILE-replace">
-            <input id="input-MYFILE-replace" name="MYFILE-action"
+          <div class="" id="radio-MYFILE-replace">
+            <input class="" id="input-MYFILE-replace" name="MYFILE-action"
                    type="radio" value="replace"/>
             <span>Replace existing file</span>
           </div>
